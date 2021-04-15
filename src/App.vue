35de171154266dabc9c951db374b22d6f3984584
erenhncr <erenhncr@gmail.com>
2021-04-15 17:11:00 +0300

    views and json-server added

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,34 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Header
+      @toggle-add-task="toggleAddTask" 
+      title="Task Tracker"
+      :showAddTask="showAddTask" />    
+    <router-view :showAddTask="showAddTask" />
+    <Footer />
   </div> 
 </template>
 
 <script>
 import Header from './components/Header';
-import Tasks from './components/Tasks';
+import Footer from './components/Footer';
+
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks,
+    Footer,
   },
   data(){
     return {
-      tasks: [],
+      showAddTask: false,
     }
   },
-  created(){
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'March 3rd at 1:30',
-        reminder: true,
-      },  
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'March 10rd at 12:30',
-        reminder: false,
-      },  
-    ]
-  },
+  methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask;
+    }    
+  }
 }
 </script>
 
